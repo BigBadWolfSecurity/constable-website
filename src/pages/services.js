4b7da4f5fb7269d9001606b0f90a28bfc4b7d2a0
Nick Stocchero <nick123pig@gmail.com>
2022-03-14 19:52:06 -0500

    initial commit

---
+++ src/pages/services.js
@@ -0,0 +1,26 @@
+import React from "react"
+import Layout from '../components/App/Layout'
+import SEO from "../components/App/seo"
+import BannerSingle from "../components/Index/BannerSingle"
+import Features from "../components/Index/Features"
+import WelcomeServices from "../components/Index/WelcomeServices"
+import Works from "../components/Index/Works"
+import About from "../components/Index/About"
+import Team from "../components/Index/Team"
+import Blog from "../components/Index/Blog"
+import VideoArea from "../components/Index/VideoArea"
+import Pricing from "../components/Index/Pricing"
+import FunFacts from "../components/Index/FunFacts"
+import Testimonials from "../components/Index/Testimonials"
+import FAQ from "../components/Index/FAQ"
+import Partner from "../components/Index/Partner"
+import Contact from "../components/Index/Contact"
+
+const IndexPage = () => (
+    <Layout>
+        <SEO title="Services" />
+        Hello!
+    </Layout>
+)
+
+export default IndexPage
